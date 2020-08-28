b8dd6e1eba0777ec4e87b2e04eebee2a976167ad
Adeel Ali <wikz@live.com>
2020-08-28 12:17:35 +0500

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './tailwind.css';
+
+import './assets/css/tailwind.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
